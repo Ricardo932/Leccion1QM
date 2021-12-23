3cdb9dfa6c924e4f4ae7c6dcb8e878c6b1c7efe9
Ricardo_05 <jose.quintom@ug.edu.ec>
2021-12-23 12:42:42 -0500

    Leccion1

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Leccion1QM',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
---
+++ src/app/home/home.page.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+  data: any;
+
+  constructor() { }
+
+  ngOnInit() {
+    fetch('./assets/data/data.json').then(res => res.json())
+    .then(json => {
+      this.data = json;
+    });
+  }
+}
